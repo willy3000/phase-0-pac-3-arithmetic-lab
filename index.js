4b048208ca7e5657e5a68d7cc7815a9f6d535a3a
Jillo Wario <willywario0@gmail.com>
2023-12-13 12:44:31 +0300

    Done with Assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+const add = (a, b) => {
+  return a + b;
+};
+const subtract = (a, b) => {
+  return a - b;
+};
+const multiply = (a, b) => {
+  return a * b;
+};
+const divide = (a, b) => {
+  return a / b;
+};
+const increment = (n) => {
+  return n + 1;
+};
+const decrement = (n) => {
+  return n - 1;
+};
+
+const makeInt = (n) => {
+  return parseInt(n, 10);
+};
+
+const preserveDecimal = (n) => {
+  return parseFloat(n)
+};
